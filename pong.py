0ebc196c56a73aa3bc0fbcf9698824a260cb64e3
MeridianPY <meridianpy@gmail.com>
2019-08-24 21:20:27 +0200

    Code restructure
    Restructured main code to use classes in order to prepare for appyling an neural network.

--- pong.py
+++ pong.py
@@ -15,192 +15,189 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 
 
-def draw_arena():
-    screen.fill((0, 0, 0))
-    # Border
-    pygame.draw.rect(screen, white, ((0, 0), (windowwidth, windowheight)), linethickness)
-    # Center line
-    pygame.draw.line(screen, white, (int(windowwidth/2), 0),
-                     (int(windowwidth/2), windowheight), int(linethickness/4 + 1))
+class Ball:
+    def __init__(self, x=windowwidth/2, y=windowheight/2, dir_x=1, dir_y=random.choice((-1, 1))):
+        self.x = x
+        self.y = y
+        self.dir_x = dir_x
+        self.dir_y = dir_y
+        self.ball = pygame.Rect(self.x, self.y, linethickness, linethickness)
+
+    # Move the Ball in the current direction.
+    def move(self):
+        self.x += self.dir_x
+        self.y += self.dir_y
+        self.ball = pygame.Rect(self.x, self.y, linethickness, linethickness)
+
+    # Draw the Ball on the screen.
+    def draw(self):
+        pygame.draw.rect(disp, white, self.ball)
+
+    # Check for collisions with walls and set paddle-state.
+    def check_collision(self, paddle1, paddle2):
+        if self.ball.top == linethickness / 2 or self.ball.bottom == (windowheight - linethickness / 2):
+            self.dir_y = self.dir_y * -1
+        elif self.ball.left == linethickness / 2:
+            self.dir_x = 0
+            self.dir_y = 0
+            paddle1.game_over = True
+        elif self.ball.right == (windowwidth - linethickness / 2):
+            self.dir_x = 0
+            self.dir_y = 0
+            paddle2.game_over = True
+
+    # Check for hit on paddle and add score.
+    def check_ball_hit(self, paddle1, paddle2):
+        if self.dir_x == -1 and paddle1.paddle.right == self.ball.left and paddle1.paddle.top < self.ball.bottom and \
+                paddle1.paddle.bottom > self.ball.top:
+            self.dir_x = self.dir_x * -1
+            paddle1.score += 1
+        elif self.dir_x == 1 and paddle2.paddle.left == self.ball.right and paddle2.paddle.top < self.ball.bottom and \
+                paddle2.paddle.bottom > self.ball.top:
+            self.dir_x = self.dir_x * -1
+            paddle2.score += 1
 
 
-def draw_paddle(paddle):
-    if paddle.bottom > windowheight - linethickness / 2:
-        paddle.bottom = windowheight - linethickness / 2
-    elif paddle.top < linethickness / 2:
-        paddle.top = linethickness / 2
-    pygame.draw.rect(screen, white, paddle)
+# Left Paddle, Player or PC
+class PaddlePC:
+    def __init__(self, x=paddleoffset, y=windowheight/2-paddlesize/2, auto_play=True):
+        self.paddle = pygame.Rect(x, y, linethickness, paddlesize)
+        self.score = 0
+        self.game_over = False
+        self.auto_play = auto_play
+
+    # Draw the paddle on the screen.
+    def draw(self):
+        if self.paddle.bottom > windowheight - linethickness / 2:
+            self.paddle.bottom = windowheight - linethickness / 2
+        elif self.paddle.top < linethickness / 2:
+            self.paddle.top = linethickness / 2
+        pygame.draw.rect(disp, white, self.paddle)
+
+    # Move player with arrow keys.
+    def move_player(self):
+        keys = pygame.key.get_pressed()
+        if keys[K_UP]:
+            self.paddle.y -= 1
+        if keys[K_DOWN]:
+            self.paddle.y += 1
+
+    # Computer, that follows the ball on the y-axis.
+    def move_computer(self, ball):
+        if ball.dir_x == 1:
+            if self.paddle.centery < (windowheight / 2):
+                self.paddle.y += 1
+            elif self.paddle.centery > (windowheight / 2):
+                self.paddle.y -= 1
+        elif ball.dir_x == -1:
+            if self.paddle.centery < ball.ball.centery:
+                self.paddle.y += 1
+            else:
+                self.paddle.y -= 1
 
 
-def draw_ball(ball):
-    pygame.draw.rect(screen, white, ball)
+# Right paddle, currently just Computer, soon to be controlled by AI
+class PaddleAI:
+    def __init__(self, x=windowwidth-linethickness-paddleoffset, y=windowheight/2-paddlesize/2):
+        self.paddle = pygame.Rect(x, y, 10, 50)
+        self.score = 0
+        self.game_over = False
+
+    # Draw the paddle to the screen.
+    def draw(self):
+        if self.paddle.bottom > windowheight - linethickness / 2:
+            self.paddle.bottom = windowheight - linethickness / 2
+        elif self.paddle.top < linethickness / 2:
+            self.paddle.top = linethickness / 2
+        pygame.draw.rect(disp, white, self.paddle)
+
+    # Move the paddle to follow the ball.
+    def move_ai(self, ball):
+        if ball.dir_x == -1:
+            if self.paddle.centery < (windowheight / 2):
+                self.paddle.y += 1
+            elif self.paddle.centery > (windowheight / 2):
+                self.paddle.y -= 1
+        elif ball.dir_x == 1:
+            if self.paddle.centery < ball.ball.centery:
+                self.paddle.y += 1
+            else:
+                self.paddle.y -= 1
 
 
-def draw_game_over(player):
-    game_over_font = pygame.font.Font('freesansbold.ttf', 90)
-    lost_font = pygame.font.Font('freesansbold.ttf', 45)
-    box = pygame.Rect(0, 0, windowwidth/2, windowheight/2)
-    box.center = (windowwidth/2, windowheight/2)
-    game_surf = game_over_font.render('GAME', True, black)
-    over_surf = game_over_font.render('OVER', True, black)
-    lost_surf = lost_font.render('Player {0} lost!'.format(player), True, white)
-    game_rect = game_surf.get_rect()
-    over_rect = over_surf.get_rect()
-    lost_rect = lost_surf.get_rect()
-    game_rect.midbottom = (windowwidth/2, windowheight/2)
-    over_rect.midtop = (windowwidth/2, windowheight/2)
-    lost_rect.midtop = (windowwidth/2, box.midbottom[1] + 10)
-    pygame.draw.rect(screen, white, box)
-    screen.blit(game_surf, game_rect)
-    screen.blit(over_surf, over_rect)
-    screen.blit(lost_surf, lost_rect)
+# Arena, draw background, scores and Game-Over-Screen.
+class Arena:
+    # Draw arena lines.
+    @staticmethod
+    def draw():
+        disp.fill((0, 0, 0))
+        # Border
+        pygame.draw.rect(disp, white, ((0, 0), (windowwidth, windowheight)), linethickness)
+        # Center line
+        pygame.draw.line(disp, white, (int(windowwidth/2), 0),
+                         (int(windowwidth/2), windowheight), int(linethickness/4 + 1))
+
+    # Draw scores.
+    @staticmethod
+    def display_score(paddle1, paddle2):
+        score1_surf = basicfont.render(str(paddle1.score), True, white)
+        score2_surf = basicfont.render(str(paddle2.score), True, white)
+        score1_rect = score1_surf.get_rect()
+        score2_rect = score2_surf.get_rect()
+        score1_rect.topleft = (150, 25)
+        score2_rect.topright = (windowwidth - 150, 25)
+        disp.blit(score1_surf, score1_rect)
+        disp.blit(score2_surf, score2_rect)
+
+    # Draw Game-Over-Screen.
+    @staticmethod
+    def game_over_screen(player):
+        game_over_font = pygame.font.Font('freesansbold.ttf', 90)
+        lost_font = pygame.font.Font('freesansbold.ttf', 45)
+        box = pygame.Rect(0, 0, windowwidth / 2, windowheight / 2)
+        box.center = (windowwidth / 2, windowheight / 2)
+        game_surf = game_over_font.render('GAME', True, black)
+        over_surf = game_over_font.render('OVER', True, black)
+        lost_surf = lost_font.render('Player {0} lost!'.format(player), True, white)
+        game_rect = game_surf.get_rect()
+        over_rect = over_surf.get_rect()
+        lost_rect = lost_surf.get_rect()
+        game_rect.midbottom = (windowwidth / 2, windowheight / 2)
+        over_rect.midtop = (windowwidth / 2, windowheight / 2)
+        lost_rect.midtop = (windowwidth / 2, box.midbottom[1] + 10)
+        pygame.draw.rect(disp, white, box)
+        disp.blit(game_surf, game_rect)
+        disp.blit(over_surf, over_rect)
+        disp.blit(lost_surf, lost_rect)
 
 
-def display_score(score1, score2):
-    score1_surf = basicfont.render(str(score1), True, white)
-    score2_surf = basicfont.render(str(score2), True, white)
-    score1_rect = score1_surf.get_rect()
-    score2_rect = score2_surf.get_rect()
-    score1_rect.topleft = (150, 25)
-    score2_rect.topright = (windowwidth - 150, 25)
-    screen.blit(score1_surf, score1_rect)
-    screen.blit(score2_surf, score2_rect)
-
-
-def move_ball(ball, ball_dir_x, ball_dir_y):
-    ball.x += ball_dir_x
-    ball.y += ball_dir_y
-    return ball
-
-
-def check_collision(ball, ball_dir_x, ball_dir_y):
-    if ball.top == linethickness/2 or ball.bottom == (windowheight - linethickness/2):
-        ball_dir_y = ball_dir_y * -1
-        game_over = False
-        player = 0
-    elif ball.left == linethickness/2:
-        ball_dir_x = 0
-        ball_dir_y = 0
-        game_over = True
-        player = 1
-    elif ball.right == (windowwidth - linethickness/2):
-        ball_dir_x = 0
-        ball_dir_y = 0
-        game_over = True
-        player = 2
-    else:
-        game_over = False
-        player = 0
-    return ball_dir_x, ball_dir_y, game_over, player
-
-
-def check_ball_hit(ball, paddle1, paddle2, ball_dir_x):
-    if ball_dir_x == -1 and paddle1.right == ball.left and paddle1.top < ball.bottom and paddle1.bottom > ball.top:
-        return -1
-    elif ball_dir_x == 1 and paddle2.left == ball.right and paddle2.top < ball.bottom and paddle2.bottom > ball.top:
-        return -1
-    else:
-        return 1
-
-
-def check_point_scored(paddle1, paddle2, score1, score2, ball, ball_dir_x):
-    if ball_dir_x == -1 and paddle1.right == ball.left and paddle1.top < ball.bottom and paddle1.bottom > ball.top:
-        score1 += 1
-        return score1, score2
-    elif ball_dir_x == 1 and paddle2.left == ball.right and paddle2.top < ball.bottom and paddle2.bottom > ball.top:
-        score2 += 1
-        return score1, score2
-    else:
-        return score1, score2
-
-
-def move_player(paddle):
-    move_ticker = 0
-    keys = pygame.key.get_pressed()
-    if keys[K_UP]:
-        if move_ticker == 0:
-            move_ticker = 10
-            paddle.y -= 1
-    if keys[K_DOWN]:
-        if move_ticker == 0:
-            move_ticker = 10
-            paddle.y += 1
-    return paddle, move_ticker
-
-
-def move_computer(ball, ball_dir_x, paddle):
-    # If ball is moving away from paddle, center bat
-    if ball_dir_x == 1:
-        if paddle.centery < (windowheight/2):
-            paddle.y += 1
-        elif paddle.centery > (windowheight/2):
-            paddle.y -= 1
-    # if ball moving towards bat, track its movement.
-    elif ball_dir_x == -1:
-        if paddle.centery < ball.centery:
-            paddle.y += 1
-        else:
-            paddle.y -= 1
-    return paddle
-
-
-def move_ai(ball, ball_dir_x, paddle):
-    # If ball is moving away from paddle, center bat
-    if ball_dir_x == -1:
-        if paddle.centery < (windowheight/2):
-            paddle.y += 1
-        elif paddle.centery > (windowheight/2):
-            paddle.y -= 1
-    # if ball moving towards bat, track its movement.
-    elif ball_dir_x == 1:
-        if paddle.centery < ball.centery:
-            paddle.y += 1
-        else:
-            paddle.y -= 1
-    return paddle
-
-
-def reset():
-    ball_x = windowwidth / 2 - linethickness / 2
-    ball_y = windowheight / 2 - linethickness / 2
-    pos_player1 = windowheight / 2 - paddlesize / 2
-    pos_player2 = windowheight / 2 - paddlesize / 2
-
-    ball_dir_x = 1
-    ball_dir_y = random.choice((-1, 1))
-
-    score1 = 0
-    score2 = 0
-
-    ball = pygame.Rect(ball_x, ball_y, linethickness, linethickness)
-    paddle1 = pygame.Rect(paddleoffset, pos_player1, linethickness, paddlesize)
-    paddle2 = pygame.Rect(windowwidth - paddleoffset - linethickness, pos_player2, linethickness, paddlesize)
-
-    return ball, ball_dir_x, ball_dir_y, paddle1, paddle2, score1, score2
-
-
+# Main game, used for testing.
 def main():
+    # Initialize game.
     pygame.init()
-    global screen
+    global disp
     global basicfont, basicfontsize
     basicfontsize = 20
     basicfont = pygame.font.Font('freesansbold.ttf', basicfontsize)
     fpsclock = pygame.time.Clock()
-    screen = pygame.display.set_mode((windowwidth, windowheight))
+    disp = pygame.display.set_mode((windowwidth, windowheight))
     pygame.display.set_caption('Pong')
 
-    auto_play = True
-    game_over = False
-    move_ticker = 0
+    # Assign classes.
+    arena = Arena()
+    paddle1 = PaddlePC()
+    paddle2 = PaddleAI()
+    ball = Ball()
 
-    ball, ball_dir_x, ball_dir_y, paddle1, paddle2, score1, score2 = reset()
+    # Draw all parts.
+    arena.draw()
+    paddle1.draw()
+    paddle2.draw()
+    ball.draw()
 
-    draw_arena()
-    draw_paddle(paddle1)
-    draw_paddle(paddle2)
-    draw_ball(ball)
-
+    # Main game loop.
     while True:
+        # PyGame events, used to get key-presses.
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -209,43 +206,38 @@ def main():
                 if event.key == pygame.K_ESCAPE:
                     while True:
                         event = pygame.event.wait()
-                        if event.type == pygame.KEYDOWN:
-                            if event.key == pygame.K_ESCAPE:
-                                break
+                        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                            break
                 if event.key == pygame.K_SPACE:
-                    if game_over:
-                        game_over = False
-                        ball, ball_dir_x, ball_dir_y, paddle1, paddle2, score1, score2 = reset()
-                    elif auto_play:
-                        auto_play = False
+                    if paddle1.auto_play:
+                        paddle1.auto_play = False
                     else:
-                        auto_play = True
+                        paddle1.auto_play = True
 
-        if auto_play:
-            paddle1 = move_computer(ball, ball_dir_x, paddle1)
-        else:
-            paddle1, move_ticker = move_player(paddle1)
+        # Move paddles.
+        if paddle1.auto_play:
+            paddle1.move_computer(ball)
+        elif not paddle1.game_over:
+            paddle1.move_player()
+        paddle2.move_ai(ball)
 
-        paddle2 = move_ai(ball, ball_dir_x, paddle2)
+        # Move ball and check for collisions or hits.
+        ball.move()
+        ball.check_collision(paddle1, paddle2)
+        ball.check_ball_hit(paddle1, paddle2)
 
-        ball = move_ball(ball, ball_dir_x, ball_dir_y)
-        ball_dir_x, ball_dir_y, game_over, player = check_collision(ball, ball_dir_x, ball_dir_y)
+        # Draw all parts and display score.
+        arena.draw()
+        paddle1.draw()
+        paddle2.draw()
+        ball.draw()
+        arena.display_score(paddle1, paddle2)
 
-        score1, score2 = check_point_scored(paddle1, paddle2, score1, score2, ball, ball_dir_x)
-        ball_dir_x = ball_dir_x * check_ball_hit(ball, paddle1, paddle2, ball_dir_x)
+        # Draw Game-Over-Screen.
+        if paddle1.game_over:
+            arena.game_over_screen(1)
 
-        draw_arena()
-        draw_paddle(paddle1)
-        draw_paddle(paddle2)
-        draw_ball(ball)
-        display_score(score1, score2)
-
-        if move_ticker > 0:
-            move_ticker -= 1
-
-        if game_over:
-            draw_game_over(player)
-
+        # Update the screen and tick the clock once.
         pygame.display.update()
         fpsclock.tick(fps)
 
