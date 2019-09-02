03ce9ded49e669083c390812b42de6cab5d3d65c
MeridianPY <lukas.romacker@t-online.de>
2019-09-02 12:28:59 +0200

    Finalized NEAT algorithm

--- pong.py
+++ pong.py
@@ -7,8 +7,8 @@ import neat
 
 fps = 288
 
-windowwidth = 600
-windowheight = 400
+width = 600
+height = 400
 linethickness = 10
 paddlesize = 50
 paddleoffset = 20
@@ -16,9 +16,13 @@ paddleoffset = 20
 black = (0, 0, 0)
 white = (255, 255, 255)
 
+gen = 0
+
+auto_play_state = True
+
 
 class Ball:
-    def __init__(self, x=windowwidth / 2 - linethickness / 2, y=windowheight / 2 - linethickness / 2,
+    def __init__(self, x=width / 2 - linethickness / 2, y=height / 2 - linethickness / 2,
                  dir_x=1, dir_y=random.choice((-1, 1))):
         self.x = x
         self.y = y
@@ -38,12 +42,12 @@ class Ball:
 
     # Check for collisions with walls and set paddle-state.
     def check_collision(self):
-        if self.ball.top == linethickness / 2 or self.ball.bottom == (windowheight - linethickness / 2):
+        if self.ball.top == linethickness / 2 or self.ball.bottom == (height - linethickness / 2):
             self.dir_y = self.dir_y * -1
         elif self.ball.left == linethickness / 2:
             self.dir_x = 0
             self.dir_y = 0
-        elif self.ball.right == (windowwidth - linethickness / 2):
+        elif self.ball.right == (width - linethickness / 2):
             self.dir_x = 0
             self.dir_y = 0
 
@@ -60,15 +64,15 @@ class Ball:
 
 
 class Paddle:
-    # Position for right paddle: x=windowwidth - linethickness - paddleoffset, y=windowheight / 2 - paddlesize / 2
-    # Position for left  paddle: x=paddleoffset, y=windowheight / 2 - paddlesize / 2
+    # Position for right paddle: x=width - linethickness - paddleoffset, y=height / 2 - paddlesize / 2
+    # Position for left  paddle: x=paddleoffset, y=height / 2 - paddlesize / 2
     def __init__(self, position='right', auto_play=False):
         if position == 'left':
             x = paddleoffset
-            y = windowheight / 2 - paddlesize / 2
+            y = height / 2 - paddlesize / 2
         elif position == 'right':
-            x = windowwidth - linethickness - paddleoffset
-            y = windowheight / 2 - paddlesize / 2
+            x = width - linethickness - paddleoffset
+            y = height / 2 - paddlesize / 2
         else:
             raise Exception('Position not defined!')
         self.position = position
@@ -79,8 +83,8 @@ class Paddle:
 
     # Draw the paddle on the screen.
     def draw(self):
-        if self.paddle.bottom > windowheight - linethickness / 2:
-            self.paddle.bottom = windowheight - linethickness / 2
+        if self.paddle.bottom > height - linethickness / 2:
+            self.paddle.bottom = height - linethickness / 2
         elif self.paddle.top < linethickness / 2:
             self.paddle.top = linethickness / 2
         outline = pygame.Rect(self.paddle.x, self.paddle.y - 1, linethickness, linethickness + 2)
@@ -92,7 +96,7 @@ class Paddle:
             ball.dir_x = 0
             ball.dir_y = 0
             self.game_over = True
-        elif self.position == 'right' and ball.ball.right == (windowwidth - linethickness / 2):
+        elif self.position == 'right' and ball.ball.right == (width - linethickness / 2):
             ball.dir_x = 0
             ball.dir_y = 0
             self.game_over = True
@@ -102,10 +106,14 @@ class Paddle:
             if self.paddle.right == ball.ball.left and \
                     self.paddle.top < ball.ball.bottom and self.paddle.bottom > ball.ball.top:
                 self.score += 1
+                return True
         elif self.position == 'right':
             if self.paddle.left == ball.ball.right \
                     and self.paddle.top < ball.ball.bottom and self.paddle.bottom > ball.ball.top:
                 self.score += 1
+                return True
+        else:
+            return False
         
     # Move player with arrow keys.
     def move_player(self):
@@ -118,9 +126,9 @@ class Paddle:
     # Computer, that follows the ball on the y-axis.
     def move_computer(self, ball):
         if ball.dir_x == 1:
-            if self.paddle.centery < (windowheight / 2):
+            if self.paddle.centery < (height / 2):
                 self.paddle.y += 1
-            elif self.paddle.centery > (windowheight / 2):
+            elif self.paddle.centery > (height / 2):
                 self.paddle.y -= 1
         elif ball.dir_x == -1:
             if self.paddle.centery < ball.ball.centery:
@@ -136,10 +144,10 @@ class Arena:
     def draw():
         disp.fill((0, 0, 0))
         # Border
-        pygame.draw.rect(disp, white, ((0, 0), (windowwidth, windowheight)), linethickness)
+        pygame.draw.rect(disp, white, ((0, 0), (width, height)), linethickness)
         # Center line
-        pygame.draw.line(disp, white, (int(windowwidth / 2), 0),
-                         (int(windowwidth / 2), windowheight), int(linethickness / 4 + 1))
+        pygame.draw.line(disp, white, (int(width / 2), 0),
+                         (int(width / 2), height), int(linethickness / 4 + 1))
 
     # Draw scores.
     @staticmethod
@@ -148,27 +156,45 @@ class Arena:
         score2_surf = basicfont.render(str(paddle_ai.score), True, white)
         score1_rect = score1_surf.get_rect()
         score2_rect = score2_surf.get_rect()
-        score1_rect.topleft = (150, 25)
-        score2_rect.topright = (windowwidth - 150, 25)
+        score1_rect.topright = (width / 2 - 50, 25)
+        score2_rect.topleft = (width / 2 + 50, 25)
         disp.blit(score1_surf, score1_rect)
         disp.blit(score2_surf, score2_rect)
+
+    @staticmethod
+    def display_text(text, position, anchor):
+        text = str(text)
+        text_surf = basicfont.render(str(text), True, white)
+        text_rect = text_surf.get_rect()
+        setattr(text_rect, anchor, position)
+        disp.blit(text_surf, text_rect)
+
+    def display_best_fitness(self, fitness_list):
+        fitness_list.sort()
+        best_fitness = fitness_list[-1]
+        if best_fitness < 0:
+            best_fitness = str(best_fitness)
+        else:
+            best_fitness = ' ' + str(best_fitness)
+        self.display_text('Best fitness: ' + best_fitness[:6],
+                          (width / 2 + 50, height - linethickness / 2 - 25), 'bottomleft')
 
     # Draw Game-Over-Screen.
     @staticmethod
     def game_over_screen(player):
         game_over_font = pygame.font.Font('freesansbold.ttf', 90)
         lost_font = pygame.font.Font('freesansbold.ttf', 45)
-        box = pygame.Rect(0, 0, windowwidth / 2, windowheight / 2)
-        box.center = (windowwidth / 2, windowheight / 2)
+        box = pygame.Rect(0, 0, width / 2, height / 2)
+        box.center = (width / 2, height / 2)
         game_surf = game_over_font.render('GAME', True, black)
         over_surf = game_over_font.render('OVER', True, black)
         lost_surf = lost_font.render('Player {0} lost!'.format(player), True, white)
         game_rect = game_surf.get_rect()
         over_rect = over_surf.get_rect()
         lost_rect = lost_surf.get_rect()
-        game_rect.midbottom = (windowwidth / 2, windowheight / 2)
-        over_rect.midtop = (windowwidth / 2, windowheight / 2)
-        lost_rect.midtop = (windowwidth / 2, box.midbottom[1] + 10)
+        game_rect.midbottom = (width / 2, height / 2)
+        over_rect.midtop = (width / 2, height / 2)
+        lost_rect.midtop = (width / 2, box.midbottom[1] + 10)
         pygame.draw.rect(disp, white, box)
         disp.blit(game_surf, game_rect)
         disp.blit(over_surf, over_rect)
@@ -177,6 +203,10 @@ class Arena:
 
 # Main game.
 def pong(genomes, config):
+    # Increment generation counter
+    global gen
+    gen += 1
+
     # Initialize NEAT
     nets = []
     ge = []
@@ -194,25 +224,27 @@ def pong(genomes, config):
     pygame.init()
     global disp
     global basicfont, basicfontsize
+    global auto_play_state
     basicfontsize = 20
     basicfont = pygame.font.Font('freesansbold.ttf', basicfontsize)
     fpsclock = pygame.time.Clock()
-    disp = pygame.display.set_mode((windowwidth, windowheight))
+    disp = pygame.display.set_mode((width, height))
     pygame.display.set_caption('Pong')
+    run = True
+    best_score = 0
 
     # Assign classes.
     arena = Arena()
-    paddle_left = Paddle(position='left', auto_play=True)
-    ball = Ball()
+    paddle_left = Paddle(position='left', auto_play=auto_play_state)
 
     # Draw all parts.
     arena.draw()
     paddle_left.draw()
     paddles_ai[0].draw()
-    ball.draw()
+    balls[0].draw()
 
     # Main game loop.
-    while True:
+    while run:
         # PyGame events, used to get key-presses.
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -220,15 +252,24 @@ def pong(genomes, config):
                 sys.exit()
             if event.type == KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    while True:
-                        event = pygame.event.wait()
-                        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                            break
+                    if paddle_left.game_over:
+                        paddle_left.game_over = False
+                        run = False
+                    else:
+                        while True:
+                            event = pygame.event.wait()
+                            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                                break
+                            if event.type == QUIT:
+                                pygame.quit()
+                                sys.exit()
                 if event.key == pygame.K_SPACE:
                     if paddle_left.auto_play:
                         paddle_left.auto_play = False
-                    else:
+                        auto_play_state = False
+                    elif not paddle_left.auto_play:
                         paddle_left.auto_play = True
+                        auto_play_state = True
 
         if len(paddles_ai) > 0:
             arena.draw()
@@ -241,14 +282,19 @@ def pong(genomes, config):
             paddle_left.check_game_over(balls[0])
             paddle_left.check_score(balls[0])
 
+            fitness_list = []
             for x, paddle_ai in enumerate(paddles_ai):
-                output = nets[x].activate((paddle_ai.paddle.y, ball.x, ball.y, ball.dir_x, ball.dir_y))
+                output = nets[x].activate((paddle_ai.paddle.y, balls[x].x, balls[x].y, balls[x].dir_x, balls[x].dir_y))
                 if output[0] > 0.25:
                     # Go up
                     paddle_ai.paddle.y -= 1
+                    ge[x].fitness -= 0.0013
+                    ge[x].fitness = round(ge[x].fitness, 3)
                 elif output[0] < -0.25:
                     # Go down
                     paddle_ai.paddle.y += 1
+                    ge[x].fitness -= 0.0013
+                    ge[x].fitness = round(ge[x].fitness, 3)
                 elif -0.25 <= output[0] <= 0.25:
                     # Stay
                     paddle_ai.paddle.y += 0
@@ -256,24 +302,36 @@ def pong(genomes, config):
 
                 balls[x].check_ball_hit(paddle_ai)
                 balls[x].check_ball_hit(paddle_left)
-                paddle_ai.check_score(balls[x])
+                if paddle_ai.check_score(balls[x]):
+                    ge[x].fitness += 1
                 paddle_ai.check_game_over(balls[x])
-                ge[x].fitness = paddle_ai.score
 
                 balls[x].check_collision()
                 balls[x].move()
                 balls[x].draw()
 
-                arena.display_score(paddle_left, paddles_ai[0])
+                best_score = paddles_ai[0]
+                fitness_list.append(ge[x].fitness)
 
                 if paddle_ai.game_over:
-                    paddle_ai.score -= 1
-                    ge[x].fitness = paddle_ai.score
+                    ge[x].fitness -= 1
                     paddles_ai.pop(x)
                     nets.pop(x)
                     balls.pop(x)
                     ge.pop(x)
                     continue
+                elif paddle_ai.score >= 20:
+                    paddles_ai.pop(x)
+                    nets.pop(x)
+                    balls.pop(x)
+                    ge.pop(x)
+                    continue
+
+            arena.display_score(paddle_left, best_score)
+            arena.display_best_fitness(fitness_list)
+            arena.display_text('Gen: ' + str(gen), (50, 25), 'topleft')
+            arena.display_text('Paddles: ' + str(len(paddles_ai)),
+                               (50, height - linethickness/2 - 25), 'bottomleft')
         else:
             paddle_left.score = 0
             break
@@ -287,7 +345,15 @@ def pong(genomes, config):
         fpsclock.tick(fps)
 
 
-def run(config):
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+
+def start_ai(config):
     config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, 
                                 neat.DefaultSpeciesSet, neat.DefaultStagnation, config)
 
@@ -297,11 +363,15 @@ def run(config):
     stats = neat.StatisticsReporter()
     p.add_reporter(stats)
 
-    winner = p.run(pong, 50)
+    winner = p.run(pong)
     print(winner)
 
 
+print('hi')
+input()
 if __name__ == '__main__':
-    local_dir = os.path.dirname(__file__)
-    config_path = os.path.join(local_dir, 'neat-config.txt')
-    run(config_path)
+    input()
+    config_path = resource_path(os.path.dirname(os.path.abspath(__file__))) + '\\neat-config.txt'
+    print(config_path)
+    input()
+    start_ai(config_path)
